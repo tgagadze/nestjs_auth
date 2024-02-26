bfcf1d110f9f688312be9188f44c9b07577b650e
Tornike <Tornike@Tornikes-MacBook-Pro.local>
2024-02-27 03:19:33 +0400

    feat: mongo, user, post modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UserModule,
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
